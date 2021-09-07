b9bb69e267cdeab89f4ab7d428494097a45495f4
mikohan <angara99@gmail.com>
2021-09-07 19:00:42 +0300

    Added client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostCreate from './components/PostCreate';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Crate Post </h1>
+      <PostCreate />
+    </div>
+  );
+};
+
+export default App;
