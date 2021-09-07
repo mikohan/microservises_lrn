fcf8a3daa984e41041ae71a1560e6ccb12b5e7f2
mikohan <angara99@gmail.com>
2021-09-07 19:30:21 +0300

    Added Post list

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PostCreate from './components/PostCreate';
+import PostList from './components/PostList';
 
 const App = () => {
   return (
     <div className="container">
       <h1>Crate Post </h1>
       <PostCreate />
+      <hr />
+      <PostList />
     </div>
   );
 };
---
+++ client/src/components/PostList.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const PostList = () => {
+  return (
+    <div>
+      <h1>Post list</h1>
+    </div>
+  );
+};
+export default PostList;
